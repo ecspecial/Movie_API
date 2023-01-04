17e56f81a2a4f7bf47c4499371989387d23ca5d1
ecspecial <106328797+ecspecial@users.noreply.github.com>
2023-01-04 23:01:12 +0300

    Prepared for Heroku and cloud mongoDB

--- index.js
+++ index.js
@@ -41,7 +41,8 @@ let auth = require('./auth.js')(app);
 const passport = require('passport');
 require('./passport.js');
 
-mongoose.connect('mongodb://127.0.0.1:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
+//mongoose.connect('mongodb://127.0.0.1:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // Write logs to log.txt using morgan package
 app.use(morgan('combined', {stream: accesLogStream}));
